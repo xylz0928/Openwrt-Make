b657dfba28a2690e1822ab31c58143fbd827b37f
然后七年 <34766756+xylz0928@users.noreply.github.com>
2026-02-24 20:52:45 +0800

    Comment out xiaorouji's Passwall package clone command
    Comment out the second git clone command for xiaorouji's Passwall package.

--- diy-batch2/diy-x86-part2.sh
+++ diy-batch2/diy-x86-part2.sh
@@ -319,7 +319,7 @@ git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-
 # Dependencies
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
-git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+# git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
 # Passwall
 rm -rf feeds/luci/applications/luci-app-passwall
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
