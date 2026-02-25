8850efcf21922afe7d430382754b24a624173ff0
然后七年 <34766756+xylz0928@users.noreply.github.com>
2026-02-25 21:29:46 +0800

    Uncomment Passwall package installation commands

--- diy-batch2/diy-24.10-6.6-part2.sh
+++ diy-batch2/diy-24.10-6.6-part2.sh
@@ -190,13 +190,13 @@ git clone https://github.com/brvphoenix/wrtbwmon package/wrtbwmon
 
 ## Add Passwall
 ## Dependencies
-# rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-# git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 ## git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
 
 ## Passwall
-# rm -rf feeds/luci/applications/luci-app-passwall
-# git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
 ## git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 ## Passwall2
 ## git clone https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
