fa16023b893fa156ad686425c8984cb0f9b0998d
然后七年 <34766756+xylz0928@users.noreply.github.com>
2026-03-14 07:40:34 +0800

    Comment out Passwall package management commands
    Comment out commands for removing packages and cloning Passwall.

--- diy-batch2/diy-24.10-6.6-part2.sh
+++ diy-batch2/diy-24.10-6.6-part2.sh
@@ -190,8 +190,8 @@ git clone https://github.com/brvphoenix/wrtbwmon package/wrtbwmon
 
 ## Add Passwall
 ## Dependencies
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
+# rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+# git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 ## git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
 
 ## Passwall
