d0de4bdef7f3deea92ae4e30915bf9c0895ad4f6
然后七年 <34766756+xylz0928@users.noreply.github.com>
2026-08-30 07:41:30 +0800

    更新 diy-x86-official-part2.sh

--- diy-batch2/diy-x86-official-part2.sh
+++ diy-batch2/diy-x86-official-part2.sh
@@ -196,9 +196,9 @@ done
 
 # rm -rf feeds/packages/net/ddns-scripts
 # rm -rf feeds/packages/net/frp
-
+rm -rf package/feeds/packages/sing-box
 # 2. ImmortalWrt 软件包（vlmcsd）
-immortalPACKS="vlmcsd"
+immortalPACKS="vlmcsd sing-box"
 # frp ddns-scripts
 for immortalPACK in $immortalPACKS; do
     safe_rm "immortalPACK/$immortalPACK"
